28e2d960413eff36352ebb4e7e7d3232bb051fe4
Nataly Carbonell <natcarbonell97@gmail.com>
2020-04-05 17:55:13 -0400

    Remove business logic stuff from middleware (moved to userService)

--- server/routes/api/middleware/signupMiddleware.js
+++ server/routes/api/middleware/signupMiddleware.js
@@ -2,10 +2,6 @@
  * Express middleware for use in the signup route.
  */
 
-const Volunteer = require('../../../models/volunteer.model');
-const Admin = require('../../../models/admin.model');
-const Organization = require('../../../models/org.model');
-
 const validateSignup = (req, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -19,60 +15,14 @@ const validateSignup = (req, res, next) => {
     if(!email) {
         return res.status(403).json('Login failed. Email field is null');
     }
-    next();
-  }
-
-const generateUserType = (req, res, next) => {
     let userType = req.body.userType;
     if(!userType) {
         console.error("No type of account/user specified by the client");
         return res.status(403).json({status: "fail", message: "Error: Must specify the type of user i.e userType: 'admin'"})
     }
-    if(userType === 'volunteer') {
-        // create volunteer and add to db
-        const newVolunteer = new Volunteer({name: req.body.name});
-        newVolunteer.save().then((res) => {
-            console.log(`New volunteer user successfully added to the db ${newVolunteer}`);
-            req.body.volunteer = newVolunteer._id;
-            req.body.admin = null;
-            next();
-        }).catch((error) => {
-            console.error(`Error saving user to database ${error}`);
-            return res.status(404).json({status: "fail", message: "Failed to generate volunteer user"});
-        });
-    }
-    if(userType === 'admin') {
-        // create org and add to db
-        const newOrganization = new Organization({orgName: req.body.orgName, orgType: req.body.orgType, location: req.body.location, orgPhone: req.body.orgPhone, orgEmail: req.body.orgEmail});
-        newOrganization.save().then((res) => {
-            console.log(`New organization user successfully added to the db ${newOrganization}`);
-
-            // create admin and save to db
-            const newAdmin = new Admin({name: req.body.adminName, adminPhone: req.body.adminPhone, organization: res._id});
-            newAdmin.save().then((result) => {
-                // add admin to organization
-                Organization.findOneAndUpdate(
-                    { _id: res._id },
-                    { $push: { admins: result._id  } }).then((doc) => {
-                        console.log("Organization admins field updated ", doc);
-                        console.log(result);
-                        req.body.admin = newAdmin._id;
-                        req.body.volunteer = null;
-                        next();
-                    }).catch(err => {
-                        console.log(err) 
-                        res.status(400).json('Uhoh, error while creating admin account')});
-            }).catch(err => {
-                console.log(err) 
-                res.status(400).json('Uhoh, error while creating admin account: ' + err)});
-        }).catch((error) => {
-            console.error(`Error saving user to database ${error}`);
-            return res.status(404).json({status: "fail", message: "Failed to generate admin user"});
-        });
-    }
-}
+    next();
+};
 
 module.exports = {
-    generateUserType,
     validateSignup
-}
+};
